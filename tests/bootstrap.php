859e96a8fe62d0c012be94745829d7f4124e9d45
Ishan Vyas <isvyas@gmail.com>
2019-10-30 11:15:03 +0530

    Desc change

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Test suite bootstrap for Mix.
+ * Test suite bootstrap for AssetMix.
  *
  * This function is used to find the location of CakePHP whether CakePHP
  * has been installed as a dependency of the plugin, or the plugin is itself
