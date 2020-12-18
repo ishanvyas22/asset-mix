9e99968ada1b3eeaeb973171b249070c64c25893
Mark Story <mark@mark-story.com>
2020-12-18 14:19:46 -0500

    Add template for react-inertia
    I updated the installation docs a tiny bit. I chose not to include
    bootstrap with inertia-react as it didn't seem necessary.

--- src/StubsPathTrait.php
+++ src/StubsPathTrait.php
@@ -58,6 +58,16 @@ trait StubsPathTrait
     }
 
     /**
+     * Returns base directory path of `inertia-react` stubs path.
+     *
+     * @return string
+     */
+    public function getBaseInertiaReactStubsPath(): string
+    {
+        return ASSET_MIX_ROOT . DS . 'stubs' . DS . 'inertia-react' . DS;
+    }
+
+    /**
      * Returns `package.json` file paths for vue.
      *
      * @return array<string>
@@ -103,11 +113,26 @@ trait StubsPathTrait
     }
 
     /**
-     * Returns `package.json` file paths for react.
+     * Returns `package.json` file paths for inertia-vue.
      *
      * @return array<string>
      */
     public function getInertiaVuePackageJsonPath(): array
+    {
+        $packageJsonPath = $this->getBaseCommonStubsPath() . 'package.json';
+
+        return [
+            'from' => $packageJsonPath,
+            'to' => ROOT . DS . basename($packageJsonPath),
+        ];
+    }
+
+    /**
+     * Returns `package.json` file paths for inertia-react.
+     *
+     * @return array<string>
+     */
+    public function getInertiaReactPackageJsonPath(): array
     {
         $packageJsonPath = $this->getBaseCommonStubsPath() . 'package.json';
 
@@ -163,13 +188,28 @@ trait StubsPathTrait
     }
 
     /**
-     * Returns `webpack.mix.js` file path for react.
+     * Returns `webpack.mix.js` file path for inertia-vue.
      *
      * @return array<string>
      */
     public function getInertiaVueWebpackMixJsPath(): array
     {
         $webpackConfigPath = $this->getBaseInertiaVueStubsPath() . 'webpack.mix.js';
+
+        return [
+            'from' => $webpackConfigPath,
+            'to' => basename($webpackConfigPath),
+        ];
+    }
+
+    /**
+     * Returns `webpack.mix.js` file path for inertia-react.
+     *
+     * @return array<string>
+     */
+    public function getInertiaReactWebpackMixJsPath(): array
+    {
+        $webpackConfigPath = $this->getBaseInertiaReactStubsPath() . 'webpack.mix.js';
 
         return [
             'from' => $webpackConfigPath,
@@ -216,6 +256,31 @@ trait StubsPathTrait
         }
 
         $assetsDirPath = $this->getBaseInertiaVueStubsPath() . $dirname;
+
+        return [
+            'from_assets' => $assetsDirPath,
+            'to_assets' => basename($assetsDirPath),
+            'to_assets_css' => basename($assetsDirPath) . DS . 'css',
+            'to_assets_js' => basename($assetsDirPath) . DS . 'js',
+            'to_assets_js_app' => basename($assetsDirPath) . DS . 'js' . DS . 'app.js',
+            'to_assets_sass' => basename($assetsDirPath) . DS . 'sass',
+            'to_assets_sass_app' => basename($assetsDirPath) . DS . 'sass' . DS . 'app.scss',
+        ];
+    }
+
+    /**
+     * Returns paths of `assets` directory files for inertia-react.
+     *
+     * @param string|null $dirname Custom directory name.
+     * @return array<string>
+     */
+    public function getInertiaReactAssetsDirPaths($dirname = null): array
+    {
+        if ($dirname === null) {
+            $dirname = AssetMixCommand::ASSETS_DIR_NAME;
+        }
+
+        $assetsDirPath = $this->getBaseInertiaReactStubsPath() . $dirname;
 
         return [
             'from_assets' => $assetsDirPath,
