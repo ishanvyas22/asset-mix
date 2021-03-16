8bbbf67f55366da7a8a112b19aa2070bff11b61b
Heverton Coneglian de Freitas <hevertonconeglian@gmail.com>
2021-03-16 12:02:51 -0300

    Fixes script tag when defer = false

--- src/View/Helper/AssetMixHelper.php
+++ src/View/Helper/AssetMixHelper.php
@@ -53,7 +53,8 @@ class AssetMixHelper extends Helper
      */
     public function script(string $url, array $options = []): ?string
     {
-        $options = array_merge($options, ['defer' => true]);
+        $defaults = ['defer' => true];
+        $options += $defaults;
 
         // Get css file path, add extension if not provided, skip if url provided
         if (strpos($url, '//') !== false) {
