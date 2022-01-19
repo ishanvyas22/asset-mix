6e0a3fdd0fe55eb5a59ca9284602b51336f0d390
Andrea Martella <52160332+Andream98@users.noreply.github.com>
2022-01-19 17:22:21 +0100

    Updates the routes.php file to remove deprecation warnings (#37)
    * Removes Cake\Routing\Router::plugin() deprecation
    
    Replaced the Router class with RouterBuilder as it became deprecated in CakePHP version 4.3
    
    * Updated CakePHP version

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,12 @@
 <?php
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
-use Cake\Routing\Route\DashedRoute;
 
-Router::plugin(
-    'AssetMix',
-    ['path' => '/asset-mix'],
-    function (RouteBuilder $routes) {
-        $routes->fallbacks(DashedRoute::class);
-    }
-);
+    use Cake\Routing\Route\DashedRoute;
+    use Cake\Routing\RouteBuilder;
+
+    /** @var RouteBuilder $routes */
+    $routes->plugin(
+        'AssetMix',
+        ['path' => '/asset-mix'],
+        function (RouteBuilder $routes) {
+            $routes->fallbacks(DashedRoute::class);
+        });
