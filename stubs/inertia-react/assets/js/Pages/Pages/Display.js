99f3c7e76752bb55dafac544117641848c1b89c6
James McDonald <james@toggen.com.au>
2023-03-31 11:00:09 +1100

    Update Inertia & React Versions

--- stubs/inertia-react/assets/js/Pages/Pages/Display.js
+++ stubs/inertia-react/assets/js/Pages/Pages/Display.js
@@ -4,6 +4,6 @@ import React from 'react';
  * This view component will be rendered when visiting /pages/greet
  * when your application has been setup with inertiajs.
  */
-export default function Greet() {
+export default function Display() {
     return <div>Hello world!</div>;
 }
